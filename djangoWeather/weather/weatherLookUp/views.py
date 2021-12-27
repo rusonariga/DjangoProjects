83464ae53bc046978a83294ec496681dccda47fc
rusonariga <gmimbrioscia@gmail.com>
2021-12-26 23:22:21 -0300

    airNow API linked

--- djangoWeather/weather/weatherLookUp/views.py
+++ djangoWeather/weather/weatherLookUp/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get(
+        "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=B8592975-548D-4B85-810D-805A201A5F4E")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error loading API data from website"
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
