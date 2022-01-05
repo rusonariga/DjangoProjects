4223efc11f72ccbb16fc3eca4cb13bb415b78e1a
rusonariga <gmimbrioscia@gmail.com>
2022-01-05 20:41:57 -0300

    api stock linked, error reading handling

--- codemy/stockMarket/stockQuote/views.py
+++ codemy/stockMarket/stockQuote/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+    # pk_9c23b434f75d428eb7753d06ca042844
+    api_request = requests.get(
+        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9c23b434f75d428eb7753d06ca042844")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error"
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
