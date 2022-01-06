bef1b00a941d05c4a3ae03d0d720598d9d079081
rusonariga <gmimbrioscia@gmail.com>
2022-01-05 21:25:42 -0300

    ticker search bar functional

--- codemy/stockMarket/stockQuote/views.py
+++ codemy/stockMarket/stockQuote/views.py
@@ -4,16 +4,21 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    # pk_9c23b434f75d428eb7753d06ca042844
-    api_request = requests.get(
-        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9c23b434f75d428eb7753d06ca042844")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error"
+    if request.method == 'POST':
+        ticker = request.POST["ticker"]
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_9c23b434f75d428eb7753d06ca042844")
 
-    return render(request, 'home.html', {'api': api})
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error"
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "enter a ticker symbol please"})
 
 
 def about(request):
