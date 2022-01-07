b2262546740a95afff82402e8708b087417b59e8
rusonariga <gmimbrioscia@gmail.com>
2022-01-07 13:07:17 -0300

    delete stock added

--- codemy/stockMarket/stockQuote/urls.py
+++ codemy/stockMarket/stockQuote/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about.html', views.about, name="about"),
     path('add_stock.html', views.add_stock, name="add_stock"),
     path('delete_stock/<stock_id>', views.delete_stock, name="delete_stock"),
+    path('remove_stock.html', views.remove_stock, name="remove_stock"),
+    path('delete_stock2/<stock_id>', views.delete_stock2, name="delete_stock2"),
 ]
--- codemy/stockMarket/stockQuote/views.py
+++ codemy/stockMarket/stockQuote/views.py
@@ -3,9 +3,13 @@ from .models import stock
 from django.contrib import messages
 from .forms import stockForm
 """
-duda 1: una vez que ya cree una columna en la base de datos, como la elimino o modifico el tag
+1: una vez que ya cree una columna en la base de datos, como la elimino o modifico el tag
 2: como hacer para cambiar en el add stock de ticker a ticker_add
-3: de donde le esta pasando el stock_id? desde el html?"""
+3: de donde le esta pasando el stock_id? desde el html?
+4. Como hacer funcional el boton de delete de la lista
+5. Como hacer distintos returns depende de quien llama (no tener 2 delete fucntions).
+6. como manejar cuando se apreta el boton buscar y no hay nada
+"""
 
 
 def home(request):
@@ -54,16 +58,18 @@ def add_stock(request):
         api_output = []
 
         for ticker_item in ticker:
-            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_9c23b434f75d428eb7753d06ca042844")
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(
+                ticker_item) + "/quote?token=pk_9c23b434f75d428eb7753d06ca042844")
             try:
                 api = json.loads(api_request.content)
+                api.update({'tickerID': ticker_item.id})
                 api_output.append(api)
                 messages.success(request, ("Successfully loaded"))
             except Exception as e:
                 api = "Error"
                 messages.error(request, ("Not found, check the ticket input"))
 
-        return render(request, 'add_stock.html', {'ticker': ticker,'api_output':api_output})
+        return render(request, 'add_stock.html', {'ticker': ticker, 'api_output': api_output})
 
 
 def delete_stock(request, stock_id):
@@ -71,3 +77,15 @@ def delete_stock(request, stock_id):
     item.delete()
     messages.success(request, ("Stock has been deleted"))
     return redirect(add_stock)
+
+
+def remove_stock(request):
+    ticker = stock.objects.all()
+    return render(request, 'remove_stock.html', {'ticker': ticker})
+
+
+def delete_stock2(request, stock_id):
+    item = stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been deleted"))
+    return redirect(remove_stock)
