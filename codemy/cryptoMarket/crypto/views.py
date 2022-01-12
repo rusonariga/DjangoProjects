d12a8633f3931eaa1e780bfc7f267205ecd26721
rusonariga <gmimbrioscia@gmail.com>
2022-01-12 00:52:49 -0300

    cryptocompare api linked

--- codemy/cryptoMarket/crypto/views.py
+++ codemy/cryptoMarket/crypto/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_request = requests.get(
+        "https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+
+    return render(request, 'home.html', {'api': api})
