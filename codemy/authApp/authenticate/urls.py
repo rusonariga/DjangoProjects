1a85b21f9116c11936216f013723e2cb8e9e2435
rusonariga <gmimbrioscia@gmail.com>
2022-01-21 16:52:30 -0300

    register form added

--- codemy/authApp/authenticate/urls.py
+++ codemy/authApp/authenticate/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.user_login, name="user_login"),
     path('logout/', views.user_logout, name="user_logout"),
+    path('registration/', views.user_registration, name="user_registration"),
 ]
--- codemy/authApp/authenticate/views.py
+++ codemy/authApp/authenticate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -28,3 +30,24 @@ def user_logout(request):
     logout(request)
     messages.success(request, ("Succesfully logged out"))
     return redirect('home')
+
+
+def user_registration(request):
+    if request.method == 'POST':
+        # form_registration = UserCreationForm(request.POST)
+        form_registration = SignUpForm(request.POST)
+        if form_registration.is_valid():
+            form_registration.save()
+            username = form_registration.cleaned_data['username']
+            password = form_registration.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Succesfully signed up"))
+            return redirect('home')
+
+    else:
+        # form_registration = UserCreationForm()
+        form_registration = SignUpForm()
+
+    context = {'form_registration': form_registration}
+    return render(request, 'authenticate/register.html', context)
